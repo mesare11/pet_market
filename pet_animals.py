415312201f5e6148176996eff7adcdea108efbbb
mesare11 <mr.mesare@gmail.com>
2020-02-11 14:37:46 +0200

    class2

---
+++ pet_animals.py
@@ -0,0 +1,6 @@
+class pet_animals(pet_goods):
+    def __init__(self, name, price, kind_pet, age):
+    pet_goods.__init__(self, name, price)
+    self.kind_pet = kind_pet
+    self.age = age
+    pass
