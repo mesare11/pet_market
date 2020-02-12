5f8a33e22717ff6e92260b92153e79a4b3a0b08a
mesare11 <mr.mesare@gmail.com>
2020-02-12 15:20:27 +0200

    main

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 from pet_goods import pet_goods
 from pet_animals import pet_animals
 from pet_items import pet_items
+from pet_cage import pet_cage
+from pet_cats import pet_cats
+from pet_feeders import pet_feeders
+from pet_rodents import pet_rodents
+from pet_stern import pet_stern
+from pet_toys import pet_toys
 
+cat1=pet_cats("Tom", 20, "Cat", "3 month", "Siamese")
+cat2=pet_cats("Joe", 15, "Cat", "4 month", "Scottish fold")
+cat3=pet_cats("Jim", 20, "Cat", "3 month", "Siamese")
+cat4=pet_cats("Fiona", 25, "Cat", "4 month", "Scottish fold")
+cat5=pet_cats("Jeremy", 30, "Cat", "2 month", "British cat")
+rat1=pet_rodents("Sam", 2, "Rodent", "1 month", "White rat")
+rat2=pet_rodents("Chloe", 3, "Rodent", "2 month", "White rat")
+rat2=pet_rodents("Pit", 3, "Rodent", "2 month", "Black rat")
+hamster1=pet_rodents("Ti", 5, "Rodent", "1 month", "Decorative hamster")
+hamster2=pet_rodents("Li", 5, "Rodent", "1 month", "Decorative hamster")
+cage1=pet_cage("Rodenta", 14, "For Rodents", "25x25x50")
+cage2=pet_cage("Rodenta", 19, "For Rodents", "40x40x50")
+cage3=pet_cage("Rodenta", 25, "For Rodents", "70x70x70")
+cage4=pet_cage("Vendy", 55, "For Birds", "70x55x150")
+cage5=pet_cage("Vendy", 70, "For Cats", "100x700x80")
+stern1=pet_stern("Vendy", 5, "For Cats", 5)
+stern2=pet_stern("Vendy", 10, "For Cats", 9)
+stern3=pet_stern("Rodenta", 4, "For Cats", 5)
+stern4=pet_stern("Rodenta", 1, "For Rodents", 1)
+stern5=pet_stern("Vendy", 2, "For Rodents", 1)
+toy1=pet_toys("Rodenta", 10, "For play", "5 years", "For Cats", "Ball")
+toy2=pet_toys("Vendy", 15, "For play", "6 years", "For Cats", "Ball")
+toy3=pet_toys("Vendy", 20, "For Run", "3 years", "For Rodents", "Hamster wheel")
+toy4=toy3=pet_toys("Rodenta", 22, "For Run", "4 years", "For Rodents", "Hamster wheel")
+toy5=pet_toys("Rodenta", 10, "For Claw", "1 year", "For Cats", "claw-point")
+feeder1=pet_feeders("Vendy", 2, "For eat", "2 years", "For Cats", "20x20x20")
+feeder2=pet_feeders("Vendy", 2, "For water", "2 years", "For Cats", "20x20x20")
+feeder3=pet_feeders("Rodenta", 3, "For water", "3 years", "For Cats", "25x25x25")
+feeder4=pet_feeders("Rodenta", 3, "For eat", "3 years", "For Cats", "25x25x25")
+feeder5=pet_feeders("Rodenta", 1, "For eat", "1 year", "For Rodents", "5x5x5")
