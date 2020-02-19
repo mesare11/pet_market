c3ab3eae17a66c56e8108e226c36ced654ae7eea
mesare11 <mr.mesare@gmail.com>
2020-02-19 15:36:27 +0200

    classes

--- pet_cage.py
+++ pet_cage.py
@@ -1,5 +1,9 @@
+from pet_goods import pet_goods
 class pet_cage(pet_goods):
     def __init__(self, name, price, cage_for_kind_animal, size):
-    pet_goods.__init__(self, name, price)
-    self.cage_for_kind_animal = cage_for_kind_animal
-    self.size = size
+        pet_goods.__init__(self, name, price)
+        self.cage_for_kind_animal = cage_for_kind_animal
+        self.size = size
+
+    def display_cage(self):
+        print(self.name, self.price, self.cage_for_kind_animal, self.size)
