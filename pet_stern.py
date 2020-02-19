96161ca5277720e804724800c50a8024b1faa978
mesare11 <mr.mesare@gmail.com>
2020-02-19 15:37:38 +0200

    classes

--- pet_stern.py
+++ pet_stern.py
@@ -1,5 +1,9 @@
+from pet_goods import pet_goods
 class pet_stern(pet_goods):
     def __init__(self, name, price, stern_for_kind_animal, weight):
-    pet_goods.__init__(self, name, price)
-    self.stern_for_kind_animal = stern_for_kind_animal
-    self.weight = weight
+        pet_goods.__init__(self, name, price)
+        self.stern_for_kind_animal = stern_for_kind_animal
+        self.weight = weight
+
+    def display_stern(self):
+        print(self.name, self.price, self.stern_for_kind_animal, self.weight)
