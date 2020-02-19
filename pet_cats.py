1cab7fb5c4b102c99ed8982fb65a04da5408e96b
mesare11 <mr.mesare@gmail.com>
2020-02-19 15:36:38 +0200

    classes

--- pet_cats.py
+++ pet_cats.py
@@ -1,5 +1,9 @@
+from pet_goods import pet_goods
+from pet_animals import pet_animals
 class pet_cats(pet_animals):
     def __init__(self, name, price, kind_pet, age, cats_breed):
-    pet_goods.__init__(self, name, price)
-    pet_animals.__init__(self, kind_pet, age)
-    self.cats_breed = cats_breed
+        pet_animals.__init__(self, name, price, kind_pet, age)
+        self.cats_breed = cats_breed
+
+    def display_cats(self):
+        print(self.name, self.price, self.kind_pet, self.age, self.cats_breed)
