59be15435ad01fb5202b14e725dcd714839b7a77
mesare11 <mr.mesare@gmail.com>
2020-02-19 15:36:55 +0200

    classes

--- pet_feeders.py
+++ pet_feeders.py
@@ -1,6 +1,10 @@
+from pet_goods import pet_goods
+from pet_items import pet_items
 class pet_feeders(pet_items):
     def __init__(self, name, price, kind_item, expiration_date, feeder_for_kind_animal, feeder_size):
-    pet_goods.__init__(self, name, price)
-    pet_toys.__init__(self, kind_item, expiration_date)
-    self.feeder_for_kind_animal = feeder_for_kind_animal
-    self.feeder_size = feeder_size
+        pet_items.__init__(self, name, price, kind_item, expiration_date)
+        self.feeder_for_kind_animal = feeder_for_kind_animal
+        self.feeder_size = feeder_size
+
+    def display_feeders(self):
+        print(self.name, self.price, self.kind_item, self.expiration_date, self.feeder_for_kind_animal, self.feeder_size)
