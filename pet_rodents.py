02d2a7f5dfece46cc017f988a8d531642f4d3bda
mesare11 <mr.mesare@gmail.com>
2020-02-19 15:37:27 +0200

    classes

--- pet_rodents.py
+++ pet_rodents.py
@@ -1,5 +1,9 @@
+from pet_goods import pet_goods
+from pet_animals import pet_animals
 class pet_rodents(pet_animals):
     def __init__(self, name, price, kind_pet, age, rodent_breed):
-    pet_goods.__init__(self, name, price)
-    pet_animals.__init__(self, kind_pet, age)
-    self.rodent_breed = rodent_breed
+        pet_animals.__init__(self, name, price, kind_pet, age)
+        self.rodent_breed = rodent_breed
+
+    def display_rodents(self):
+        print(self.name, self.price, self.kind_pet, self.age, self.rodent_breed)
