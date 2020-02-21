311ff8cde3141ace407869559aa22a0cbffff1ab
mesare11 <mr.mesare@gmail.com>
2020-02-21 12:58:48 +0200

    array

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pet_feeders import pet_feeders
 from pet_rodents import pet_rodents
 from pet_stern import pet_stern
 from pet_toys import pet_toys
+from array import array
 cat1=pet_cats("Tom", 20, "Cat", "3 month", "Siamese")
 cat2=pet_cats("Joe", 15, "Cat", "4 month", "Scottish fold")
 cat3=pet_cats("Jim", 20, "Cat", "3 month", "Siamese")
@@ -37,8 +38,8 @@ feeder2=pet_feeders("Vendy", 2, "For water", "2 years", "For Cats", "20x20x20")
 feeder3=pet_feeders("Rodenta", 3, "For water", "3 years", "For Cats", "25x25x25")
 feeder4=pet_feeders("Rodenta", 3, "For eat", "3 years", "For Cats", "25x25x25")
 feeder5=pet_feeders("Rodenta", 1, "For eat", "1 year", "For Rodents", "5x5x5")
-i=cat1.display_prices()+cat2.display_prices()+cat3.display_prices()+cat4.display_prices()+cat5.display_prices()+rat1.display_prices()+rat2.display_prices()+rat3.display_prices()+hamster1.display_prices()+hamster2.display_prices()+cage1.display_prices()+cage2.display_prices()+cage3.display_prices()+cage4.display_prices()+cage5.display_prices()+stern1.display_prices()+stern2.display_prices()+stern3.display_prices()+stern4.display_prices()+stern5.display_prices()+toy1.display_prices()+toy2.display_prices()+toy3.display_prices()+toy4.display_prices()+toy5.display_prices()+feeder1.display_prices()+feeder2.display_prices()+feeder3.display_prices()+feeder4.display_prices()+feeder5.display_prices()
-print('Стоимость всех товаров '+str(i))
+prices=array('l',[cat1.display_prices(),cat2.display_prices(),cat3.display_prices(),cat4.display_prices(),cat5.display_prices(),rat1.display_prices(),rat2.display_prices(),rat3.display_prices(),hamster1.display_prices(),hamster2.display_prices(),cage1.display_prices(),cage2.display_prices(),cage3.display_prices(),cage4.display_prices(),cage5.display_prices(),stern1.display_prices(),stern2.display_prices(),stern3.display_prices(),stern4.display_prices(),stern5.display_prices(),toy1.display_prices(),toy2.display_prices(),toy3.display_prices(),toy4.display_prices(),toy5.display_prices(),feeder1.display_prices(),feeder2.display_prices(),feeder3.display_prices(),feeder4.display_prices(),feeder5.display_prices()])
+print("The total value of all goods "+format(sum(prices))+"$")
 toy1.display_toys(),toy2.display_toys(),toy3.display_toys(),toy4.display_toys(),toy5.display_toys()
 stern1.display_stern(),stern2.display_stern(),stern3.display_stern(),stern4.display_stern(),stern5.display_stern()
 rat1.display_rodents(), rat2.display_rodents(),rat3.display_rodents(), hamster1.display_rodents(), hamster2.display_rodents()
