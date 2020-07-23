f3c8656bda0869ab6756a98ead6801f5bbd06e20
mesare11 <mr.mesare@gmail.com>
2020-07-23 15:14:58 +0300

    not full

--- pet_cats.py
+++ pet_cats.py
@@ -7,3 +7,16 @@ class pet_cats(pet_animals):
 
     def display_cats(self):
         print(self.name, self.price, self.kind_pet, self.age, self.cats_breed)
+
+    def add_cat():
+        goods_array=[]
+        name=input("Please input name goods: ")
+        price=int(input("Please input price goods: "))
+        kind_pet=input("Please input kind of pet: ")
+        age=input("Please input age pet: ")
+        cats_breed=input("Please input cats breed: ")
+        goods_array.append(name)
+        goods_array.append(price)
+        goods_array.append(kind_pet)
+        goods_array.append(age)
+        goods_array.append(cats_breed)
