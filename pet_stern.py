65a19fdcc1b69396b29051c16ed37ba043b0d0ff
mesare11 <mr.mesare@gmail.com>
2020-07-23 15:15:48 +0300

    not full

--- pet_stern.py
+++ pet_stern.py
@@ -7,3 +7,14 @@ class pet_stern(pet_goods):
 
     def display_stern(self):
         print(self.name, self.price, self.stern_for_kind_animal, self.weight)
+
+    def add_stern():
+        goods_array=[]
+        name=input("Please input name goods: ")
+        price=int(input("Please input price goods: "))
+        stern_for_kind_animal=input("Please input stern for kind animal: ")
+        weight=input("Please input weight: ")
+        goods_array.append(name)
+        goods_array.append(price)
+        goods_array.append(stern_for_kind_animal)
+        goods_array.append(weight)
