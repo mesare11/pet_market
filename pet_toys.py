c8e4006e578898c381be6f76cf6e05af39b94862
mesare11 <mr.mesare@gmail.com>
2020-07-23 15:16:01 +0300

    not full

--- pet_toys.py
+++ pet_toys.py
@@ -8,3 +8,18 @@ class pet_toys(pet_items):
 
     def display_toys(self):
         print(self.name, self.price, self.kind_item, self.expiration_date, self.toy_for_kind_animal, self.kind_of_toy)
+
+    def add_toy():
+        goods_array=[]
+        name=input("Please input name goods: ")
+        price=int(input("Please input price goods: "))
+        kind_item=input("Please input kind item: ")
+        expiration_date=input("Please input expiration date: ")
+        toy_for_kind_animal=input("Please input toy for kind animal: ")
+        kind_of_toy=input("Please input kind of toy: ")
+        goods_array.append(name)
+        goods_array.append(price)
+        goods_array.append(kind_item)
+        goods_array.append(expiration_date)
+        goods_array.append(toy_for_kind_animal)
+        goods_array.append(kind_of_toy)
