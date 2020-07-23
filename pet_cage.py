ea36d606f354f801fc60af05e2be6ece91faaeb7
mesare11 <mr.mesare@gmail.com>
2020-07-23 15:14:31 +0300

    not full

--- pet_cage.py
+++ pet_cage.py
@@ -7,3 +7,14 @@ class pet_cage(pet_goods):
 
     def display_cage(self):
         print(self.name, self.price, self.cage_for_kind_animal, self.size)
+
+    def add_cage():
+        goods_array=[]
+        name=input("Please input name goods: ")
+        price=int(input("Please input price goods: "))
+        cage_for_kind_animal=input("Please input cage for kind animal: ")
+        size=input("Please input size: ")
+        goods_array.append(name)
+        goods_array.append(price)
+        goods_array.append(cage_for_kind_animal)
+        goods_array.append(size)
